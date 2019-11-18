e9885b8a6cc6b575ce9661136a22d10dd8f2b733
expulsado <aleksandr.s.v.4@gmail.com>
2019-11-18 21:24:25 +0300

    working version of task

--- test.py
+++ test.py
@@ -3,55 +3,60 @@ import requests
 import json
 import datetime
 import time
+
+
+bizon_token = 'Bxt5aOu9rSlgKq6dd9rre-F9TduqrBxGYca_OqrBgQK9pdO5r'
+
+bizon_headers = {'X-Token': bizon_token}
+
 url = "https://online.bizon365.ru/api/v1/webinars/reports/getlist?limit=100"
 
-answer_from_server =  requests.get(url) 
+answer_from_server =  requests.get(url, headers = bizon_headers) 
 
 json_data = json.loads(answer_from_server.text) 
  
 
  
- #пока создал предположительный JSON файл, который должен прийти с сервера
- #тк получить с сервера нет возможности
- #далее следующие две строчки нужно удалить 
-json_data =  '{"skip": 0, "limit": 100, "count": 100, "list": [{"name": "123", "webinarId": "webid", "type": "LiveWebinars", "created": "2013-02-25T18:25:10", "count1": 1234, "count2": 321}]}'
-json_data = json.loads(json_data)
+#пока создал предположительный JSON файл, который должен прийти с сервера
+#тк получить с сервера нет возможности
+#далее следующие две строчки нужно удалить 
+#json_data = '{"skip": 0, "limit": 100, "count": 100, "list": [{"name": "123", "webinarId": "webid", "type": "LiveWebinars", "created": "2013-02-25T18:25:10", "count1": 1234, "count2": 321}]}'
+#json_data = json.loads(json_data)
 
 
 
-number_of_elements_in_list =0
-for results in json_data['list']:
-    number_of_elements_in_list+=1
 
 
-number_of_viewers = 0 
-number_of_live_webs = 0
-number_of_auto_webs = 0
+elements_in_list = len(json_data['list'])
+viewers = 0 
+live_webs = 0
+auto_webs = 0
 duration_of_webinar = 0
-index_of_element_in_list = 0
+index = 0
 
-while index_of_element_in_list < number_of_elements_in_list:
-    duration_of_webinar += json_data['list'][index_of_element_in_list]['count2']
-    if json_data['list'][index_of_element_in_list]['type'] == "LiveWebinars":
-        number_of_live_webs+=1
-        number_of_viewers += json_data['list'][index_of_element_in_list]['count1']
+while index < elements_in_list:
+    duration_of_webinar += json_data['list'][index]['count2']
+    if json_data['list'][index]['type'] == "LiveWebinars":
+        live_webs += 1
+        viewers += json_data['list'][index]['count1']
        
     else:
-        number_of_auto_webs+=1
+        auto_webs += 1
         
-    index_of_element_in_list+=1
+    index += 1
 
-average_number_of_viewers = number_of_viewers / number_of_live_webs
+avg_viewers = round(viewers / live_webs)
 
-average_duration_of_webinar = duration_of_webinar / (number_of_live_webs + number_of_auto_webs)
+avg_duration_of_webinar = duration_of_webinar / (live_webs + auto_webs)
 
-seconds = average_duration_of_webinar * 60  
+seconds = round(avg_duration_of_webinar, 3) * 60  
 
 
-print("Количество живых вебинаров: " + str(number_of_live_webs))
 
-print("Количество авто вебинаров: " + str(number_of_auto_webs))
+print("Количество живых вебинаров: " + str(live_webs))
 
-print("Среднее количество участников на вебинарах: " + str(average_number_of_viewers))
+print("Количество авто вебинаров: " + str(auto_webs))
+
+print("Среднее количество участников на вебинарах: " + str(avg_viewers))
 
 print("Средняя длина вебинара: " + str(datetime.timedelta(0, seconds)))
